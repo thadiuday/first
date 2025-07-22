e8dba7362db0075e0f3d2aba56c194c86ae341bc
thadiuday <tadiuday999@gmail.com>
2025-07-22 18:51:34 +0530

    Update script.js

--- script.js
+++ script.js
@@ -55,4 +55,21 @@ document.addEventListener("DOMContentLoaded", function () {
     navLinks.classList.toggle("active");
   });
 });
+// Back to Top Button Logic
+const backToTopBtn = document.getElementById("backToTop");
+
+window.addEventListener("scroll", () => {
+  if (window.scrollY > 200) {
+    backToTopBtn.style.display = "block";
+  } else {
+    backToTopBtn.style.display = "none";
+  }
+});
+
+backToTopBtn.addEventListener("click", () => {
+  window.scrollTo({
+    top: 0,
+    behavior: "smooth"
+  });
+});
 
